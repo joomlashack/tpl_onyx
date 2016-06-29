0e80cd12200294420587dc016796255e8d57fa18
Valentín <htmgarcia@gmail.com>
2016-06-29 13:04:19 -0500

    Toolbar setting

--- code/functions.php
+++ code/functions.php
@@ -1,3 +1,5 @@
 <?php
 // Restrict Access to within Joomla
 defined('_JEXEC') or die('Restricted access');
+
+$onyxToolbarDisplayed = $this->params->get('onyx_toolbar_displayed', 'navbar-fixed-top');
