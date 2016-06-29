5f4a502c7965bfea8b3d0bc89f0033eaafaa0d25
Valentín <htmgarcia@gmail.com>
2016-06-29 12:22:32 -0500

    Responsive fixes

--- code/template.php
+++ code/template.php
@@ -187,7 +187,7 @@ defined('_JEXEC') or die('Restricted access');
 					:
 				?>
 				<!-- bottom-menu -->
-				<w:nav containerClass="<?php echo $wrightContainerClass ?>" rowClass="<?php echo $wrightGridMode ?>" name="bottom-menu" wrapClass="navbar-inverse navbar-transparent" />
+				<w:nav containerClass="<?php echo $wrightContainerClass ?>" rowClass="<?php echo $wrightGridMode ?>" name="bottom-menu" wrapClass="navbar-inverse" />
 				<?php
 					endif;
 				?>
