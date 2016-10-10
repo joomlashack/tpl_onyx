b04168625fb3851e35234836b5d2285c22bf6e54
jmarkos <jkmarcosb@gmail.com>
2016-10-10 15:33:27 -0600

    alignmet debug position

--- code/template.php
+++ code/template.php
@@ -221,8 +221,6 @@ defined('_JEXEC') or die('Restricted access');
 				</div>
 		   </footer>
 		</div>
-
-        <w:module type="none" name="debug" chrome="none" />
-
+		<w:module type="none" name="debug" chrome="none" />
 	</body>
 </html>
